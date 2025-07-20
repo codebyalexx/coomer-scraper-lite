4944112d16f3169fc9189a96e2b552fd51c92e6b
codebyalexx <a.legal.contact@gmail.com>
2025-07-20 10:26:22 +0200

    :sparkles: Added seed action route

---
+++ src/api/controllers/actions.controllers.js
@@ -0,0 +1,135 @@
+import prisma from "../../lib/prisma.js";
+import redisClient from "../../lib/redis.js";
+import { getAllArtistPosts } from "../../lib/coomer-api.js";
+import path from "path";
+import fs from "fs";
+import logger from "../../lib/logger.js";
+import pLimit from "p-limit";
+import { downloadFile } from "../../lib/downloader.js";
+import { getPostContent } from "../../lib/coomer-api.js";
+
+const seed = async (req, res) => {
+  try {
+    let postSelectionLimit = 150;
+    const uniqueArtists = await prisma.artist.findMany();
+    const postSelectionLimitKey = await redisClient.get("post-selection-limit");
+    if (postSelectionLimitKey)
+      postSelectionLimit = parseInt(postSelectionLimitKey);
+
+    for (const artist of uniqueArtists) {
+      try {
+        const posts = await getAllArtistPosts(artist.url);
+
+        let selectedPosts =
+          posts.length > postSelectionLimit
+            ? posts.slice(0, postSelectionLimit)
+            : posts;
+
+        if (artist.isException) {
+          selectedPosts = posts;
+        }
+
+        const postLimit = pLimit(4);
+
+        const postTasks = selectedPosts.map((post) =>
+          postLimit(async () => {
+            try {
+              const postContent = await getPostContent(artist.url, post.id);
+
+              let attachments = [];
+              if (postContent?.post?.attachments)
+                attachments = [...attachments, ...postContent.post.attachments];
+              if (postContent?.videos)
+                attachments = [...attachments, ...postContent.videos];
+
+              let postDB = await prisma.post.findFirst({
+                where: {
+                  identifier: post.id,
+                  artistId: artist.id,
+                },
+              });
+
+              if (!postDB)
+                postDB = await prisma.post.create({
+                  data: {
+                    identifier: post.id,
+                    artistId: artist.id,
+                  },
+                });
+
+              const parsedAttachments = attachments.map((attachment) => {
+                return {
+                  url: `https://coomer.su/data${attachment.path}`,
+                  path: "/data" + attachment.path,
+                  filename: attachment.name,
+                  outputPath: path.join("/app/downloads/", artist.identifier),
+                  outputFilename: attachment.name,
+                  outputFilePath: path.join(
+                    "/app/downloads/",
+                    artist.identifier,
+                    attachment.name
+                  ),
+                };
+              });
+
+              const attachmentLimit = pLimit(2);
+
+              const attachmentTasks = parsedAttachments.map((attachment) =>
+                attachmentLimit(async () => {
+                  try {
+                    if (fs.existsSync(attachment.outputFilePath)) {
+                      let fileDB = await prisma.file.findFirst({
+                        where: {
+                          filename: attachment.filename,
+                          postId: postDB.id,
+                          artistId: artist.id,
+                        },
+                      });
+
+                      if (!fileDB)
+                        fileDB = await prisma.file.create({
+                          data: {
+                            url: attachment.url,
+                            filename: attachment.filename,
+                            postId: postDB.id,
+                            artistId: artist.id,
+                          },
+                        });
+                    }
+                  } catch (e) {
+                    logger.error(
+                      `Failed to process seeding attachment ${
+                        attachment.filename
+                      }, error: ${e.message || "no error message"}`
+                    );
+                  }
+                })
+              );
+
+              await Promise.all(attachmentTasks);
+            } catch (e) {
+              logger.error(
+                `Failed to process post seeding ${post.id}, error: ${
+                  e.message || "no error message"
+                }`
+              );
+            }
+          })
+        );
+
+        await Promise.all(postTasks);
+      } catch (e) {
+        logger.error(
+          `Failed to process artist seeding ${artist.name}, error: ${
+            e.message || "no error message"
+          }`
+        );
+      }
+    }
+    res.status(200).json({ message: "Seed successful" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { seed };
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import helloWorldRoutes from "./hello-world.routes.js";
 import artistRoutes from "./artist.routes.js";
+import actionsRoutes from "./actions.routes.js";
 
 const router = new express.Router();
 
 router.use("/hello-world", helloWorldRoutes);
 router.use("/artists", artistRoutes);
+router.use("/actions", actionsRoutes);
 
 export default router;
